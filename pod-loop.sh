f77c38f08c6dbcc7269081a416e582e899d52880
kkgty <106314238+kkgty@users.noreply.github.com>
2022-05-26 20:27:07 +0800

    Update pod-loop.sh

--- pod-loop.sh
+++ pod-loop.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-ns=tunnel
 
-for i in {1..10}
-do
-    # echo router-"${i}"
-    # mkdir config/router-"${i}"
-    kubectl exec -n ${ns} router-"${i}" -- hostname
-    # kubectl exec -n ${ns} router-"${i}" -- ls /etc/frr | grep frr.conf
-    # kubectl exec -n ${ns} router-"${i}" -- vtysh -c "write integrated"
-    kubectl cp -n ${ns} router-"${i}":etc/frr/frr.conf config/router-"${i}"/frr.conf
-    kubectl cp -n ${ns} router-"${i}":etc/frr/daemons config/router-"${i}"/daemons
-done
+for i in {1..10};do;kubectl create configmap -n tunnel --from-file=router-"${i}" router-"${i}";done;
